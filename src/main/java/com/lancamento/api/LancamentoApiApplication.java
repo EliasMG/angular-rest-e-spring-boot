a014545b545cb8b511bbbb2f468d1315777035bd
EliasMG <elias.b.tec@gmail.com>
2017-09-07 20:27:33 -0300

    Primeiro commit

---
+++ src/main/java/com/lancamento/api/LancamentoApiApplication.java
@@ -0,0 +1,12 @@
+package com.lancamento.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentoApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lancamento/api/LancamentoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lancamento.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LancamentoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
