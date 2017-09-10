7980f57d9c5238fdd87a51e9d9505626f41ec0e9
EliasMG <elias.b.tec@gmail.com>
2017-09-10 10:23:15 -0300

    Consultando o primeiro recurso com GET

---
+++ src/main/java/com/lancamento/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.lancamento.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lancamento.api.model.Categoria;
+import com.lancamento.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+}
