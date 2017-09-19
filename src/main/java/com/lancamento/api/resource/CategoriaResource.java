56ebe94ab9d4c0491d913118b8a0786a81168931
EliasMG <elias.b.tec@gmail.com>
2017-09-18 22:41:12 -0300

    Implementando o método DELETE para remover uma categoria

--- src/main/java/com/lancamento/api/resource/CategoriaResource.java
+++ src/main/java/com/lancamento/api/resource/CategoriaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lancamento.api.event.RecursoCriadoEvent;
@@ -45,5 +47,11 @@ public class CategoriaResource {
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
 		return categoriaRepository.exists(codigo) ? ResponseEntity.ok().body(categoriaRepository.findOne(codigo)) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		categoriaRepository.delete(codigo);
 	}
 }
