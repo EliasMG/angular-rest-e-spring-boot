a3f8a82dcda951f96f5fa8cc3f5f910c42bd20bb
EliasMG <elias.b.tec@gmail.com>
2017-10-15 07:26:43 -0300

    Usando Profiles do Spring

--- src/main/java/com/lancamento/api/LancamentoApiApplication.java
+++ src/main/java/com/lancamento/api/LancamentoApiApplication.java
@@ -2,8 +2,12 @@ package com.lancamento.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.lancamento.api.config.property.LancamentoApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LancamentoApiProperty.class)
 public class LancamentoApiApplication {
 
 	public static void main(String[] args) {
